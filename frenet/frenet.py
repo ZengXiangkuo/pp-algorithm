9277a1d871c4bfd75c9ee0ff73931b76b74f943d
zengxiangkuo <zengxiangkuo@outlook.com>
2020-09-13 23:52:58 +0800

    remove fsolve and add spline

--- frenet/frenet.py
+++ frenet/frenet.py
@@ -2,8 +2,56 @@
 import math
 
 import numpy as np
-from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
+
+
+def spline(tt, yy):
+    t, a, b, c, d = tt, [iy for iy in yy], [], [], []
+
+    n = len(tt)
+    h = np.diff(tt)
+
+    # matrix a
+    mat_a = np.zeros((n, n))
+    mat_a[0, 0] = 1.0
+    for i in range(n - 1):
+        if i != (n - 2):
+            mat_a[i + 1, i + 1] = 2.0 * (h[i] + h[i + 1])
+        mat_a[i + 1, i] = h[i]
+        mat_a[i, i + 1] = h[i]
+    mat_a[0, 1] = 0.0
+    mat_a[n - 1, n - 2] = 0.0
+    mat_a[n - 1, n - 1] = 1.0
+
+    # matrix b
+    mat_b = np.zeros((n, 1))
+    for i in range(n - 2):
+        mat_b[i + 1, 0] = 3.0 * ((a[i + 2] - a[i + 1]) / h[i + 1] - (a[i + 1] - a[i]) / h[i])
+    mat_x = np.linalg.inv(np.mat(mat_a)) * np.mat(mat_b)
+    c = [mat_x[i, 0] for i in range(n)]
+
+    for i in range(n - 1):
+        d.append((c[i + 1] - c[i]) / (3.0 * h[i]))
+        tb = (a[i + 1] - a[i]) / h[i] - h[i] * (c[i + 1] + 2.0 * c[i]) / 3.0
+        b.append(tb)
+
+    return [t, a, b, c, d]
+
+
+def spline_fn(pt, pa, pb, pc, pd, t):
+    idx = 0
+
+    while (idx < len(pt)) and (t > pt[idx]):
+        idx += 1
+
+    idx -= 1
+    if idx >= len(pt) - 1:
+        idx = len(pt) - 2
+    elif idx < 0:
+        idx = 0
+
+    dt = t - pt[idx]
+    return pa[idx] + pb[idx] * dt + pc[idx] * dt ** 2 + pd[idx] * dt ** 3
 
 
 def bezier_fn(pa, pb, pc, pd, t):
@@ -14,132 +62,129 @@ def bezier_fn(pa, pb, pc, pd, t):
     return a + b + c + d
 
 
-def vec_norm(v):
-    return np.sqrt(v[0] * v[0] + v[1] * v[1])
+class LaneData(object):
+
+    def __init__(self):
+
+        self.num = 0
+        self.lane_xy = np.array([])
+        self.lane_s = np.array([])
+        self.lane_l = np.array([])
+
+        self.lane_d = np.array([])
+        self.lane_el = np.array([])
+        self.lane_er = np.array([])
+
+    def load(self, xys, ds):
+        assert len(xys) == len(ds)
+
+        self.num = len(xys)
+        self.lane_xy = np.array(xys)
+        self.lane_l = np.array(ds)
+        self.lane_s = np.zeros((self.num, 1))
+
+        self.lane_d = np.zeros((self.num, 2))
+
+        self.lane_el = np.zeros((self.num, 2))
+        self.lane_er = np.zeros((self.num, 2))
+
+        for i in range(1, self.num):
+            direct = self.lane_xy[i] - self.lane_xy[i - 1]
+
+            self.lane_s[i] = self.lane_s[i - 1] + np.linalg.norm(direct)
+            self.lane_d[i] = np.array(direct / np.linalg.norm(direct))
+
+        self.lane_d[0] = self.lane_d[1]
+
+        for i in range(self.num):
+            l_direct = np.array([self.lane_d[i][1], -self.lane_d[i][0]])
+            self.lane_el[i] = self.lane_xy[i] - self.lane_l[i] * l_direct
+            self.lane_er[i] = self.lane_xy[i] + self.lane_l[i] * l_direct
+
+    def frenet2xy(self, s, l):
+        index = 0
+        while index < self.num - 1 and self.lane_s[index + 1] < s:
+            index += 1
+        ds = s - self.lane_s[index]
+        x_direct = self.lane_d[index]
+        y_direct = np.array([-x_direct[1], x_direct[0]])
+        return self.lane_xy[index] + ds * x_direct + l * y_direct
 
 
-# normalize
-def vec_normalize(v):
-    return v / np.sqrt(v[0] * v[0] + v[1] * v[1])
+def quartic_polynomials(s0, sf):
+    t0, y0, y0_d1, y0_d2 = s0
+    tf, yf_d1, yf_d2 = sf
+    a = [0] * 6
+    a[0] = y0
+    a[1] = y0_d1
+    a[2] = 0.5 * y0_d2
+    dt = tf - t0
+    mat_a = [[3 * dt ** 2, 4 * dt ** 3, 5 * dt ** 4],
+             [6 * dt, 12 * dt ** 2, 20 * dt ** 3]]
+    mat_b = [[yf_d1 - a[1] - 2 * a[2] * dt],
+             [yf_d2 - 2 * a[2]]]
+    mat_x = np.linalg.inv(np.mat(mat_a)) * np.mat(mat_b)
+    a[3] = mat_x[0, 0]
+    a[4] = mat_x[1, 0]
+    return a
 
 
-# right_normal
-def vec_right_normal(v):
-    return np.array([v[1], -v[0]])
-
-
-def vec_rate(v):
-    return np.inf if v[0] == 0 else v[1] / v[0]
-
-
-def vec_add(v1, v2, rate1=1, rate2=1):
-    return [v1[0] * rate1 + v2[0] * rate2, v1[1] * rate1 + v2[1] * rate2]
+def quint_polynomials(s0, sf):
+    t0, y0, y0_d1, y0_d2 = s0
+    tf, yf, yf_d1, yf_d2 = sf
+    a = [0] * 6
+    a[0] = y0
+    a[1] = y0_d1
+    a[2] = 0.5 * y0_d2
+    dt = tf - t0
+    mat_a = [[dt ** 3, dt ** 4, dt ** 5],
+             [3 * dt ** 2, 4 * dt ** 3, 5 * dt ** 4],
+             [6 * dt, 12 * dt ** 2, 20 * dt ** 3]]
+    mat_b = [[yf - a[0] - a[1] * dt - a[2] * dt ** 2],
+             [yf_d1 - a[1] - 2 * a[2] * dt],
+             [yf_d2 - 2 * a[2]]]
+    mat_x = np.linalg.inv(np.mat(mat_a)) * np.mat(mat_b)
+    a[3] = mat_x[0, 0]
+    a[4] = mat_x[1, 0]
+    a[5] = mat_x[2, 0]
+    return a
 
 
 def compute_curve_params(params):
-    src_s = params[0]
-    src_l = params[1]
-    src_yaw = params[2]
-
-    dst_s = params[3]
-    dst_l = params[4]
-    dst_yaw = params[5]
-
-    def constraint(xx):
-        assert len(xx) == 12
-
-        trajectory_s = np.poly1d(xx[0:6])
-        trajectory_s_d1 = trajectory_s.deriv(1)
-        trajectory_s_d2 = trajectory_s.deriv(2)
-
-        trajectory_l = np.poly1d(xx[6:12])
-        trajectory_l_d1 = trajectory_l.deriv(1)
-        trajectory_l_d2 = trajectory_l.deriv(2)
-
-        ret = [0] * 12
-
-        ret[0] = trajectory_s(0) - src_s
-        ret[1] = trajectory_s_d1(0) - 100
-        ret[2] = trajectory_s_d2(0) - 0
-
-        ret[3] = trajectory_s(1) - dst_s
-        ret[4] = trajectory_s_d1(1) - 100
-        ret[5] = trajectory_s_d2(1) - 0
-
-        ret[6] = trajectory_l(0) - src_l
-        ret[7] = trajectory_l_d1(0) - 100 * math.tan(src_yaw)
-        ret[8] = trajectory_l_d2(0) - 0
-
-        ret[9] = trajectory_l(1) - dst_l
-        ret[10] = trajectory_l_d1(1) - 100 * math.tan(dst_yaw)
-        ret[11] = trajectory_l_d2(1) - 0
-
-        return ret
-
-    return fsolve(constraint, np.array([0] * 12))
+    src_s, src_l, src_yaw, dst_s, dst_l, dst_yaw = params
+    sa = quint_polynomials([0, src_s, 100 * math.cos(src_yaw), 0],
+                           [1, dst_s, 100 * math.cos(dst_yaw), 0])
+    la = quint_polynomials([0, src_l, 100 * math.sin(src_yaw), 0],
+                           [1, dst_l, 100 * math.sin(dst_yaw), 0])
+    return sa[::-1] + la[::-1]
 
 
-# create lane map data by bezier
+# create fake lane data by bezier curve function.
 num = 100
-refs = [[0, 0], [100, 0], [0, 100], [150, 100]]
+refs = [[0, 0], [100, 0], [0, 100], [200, 100]]
 refs = [np.array(p) for p in refs]
 
-# central axis
-lane_xy = [bezier_fn(*refs, i / num) for i in range(num)]
-
-# lane direction
-lane_d = [0] * num
-
-# lane s
-lane_s = [0] * num
-
-# lane l
-lane_l = [100] * num
-
-for i in range(1, num):
-    v = vec_add(lane_xy[i], lane_xy[i - 1], 1, -1)
-    lane_s[i] = lane_s[i - 1] + vec_norm(v)
-    lane_d[i] = vec_normalize(np.array(v))
-lane_d[0] = lane_d[1]
-
-lane_e_l = [0] * num
-lane_e_r = [0] * num
-for i in range(num):
-    y_direct = vec_right_normal(lane_d[i])
-    lane_e_l[i] = vec_add(lane_xy[i], y_direct, 1, -20)
-    lane_e_r[i] = vec_add(lane_xy[i], y_direct, 1, 20)
-
-
-def frenet2xy(s, l):
-    index = 0
-    while index < num - 1 and lane_s[index + 1] < s:
-        index += 1
-    ds = s - lane_s[index]
-    x_direct = lane_d[index]
-    y_direct = vec_right_normal([-x_direct[0], -x_direct[1]])
-    pos = vec_add(lane_xy[index], x_direct, 1, ds)
-    return vec_add(pos, y_direct, 1, l)
-
+lane_data = LaneData()
+lane_data.load([bezier_fn(*refs, i / num) for i in range(num)], [20] * num)
 
 # display it.
 plt.figure()
-plt.grid('on')
 plt.axis('equal')
 
-# draw bezier curve
-plt.plot([v[0] for v in lane_xy], [v[1] for v in lane_xy], '-k', lw=1)
-plt.plot([v[0] for v in lane_e_l], [v[1] for v in lane_e_l], '-b', lw=1)
-plt.plot([v[0] for v in lane_e_r], [v[1] for v in lane_e_r], '-b', lw=1)
+# draw fake lane
+plt.plot([v[0] for v in lane_data.lane_xy], [v[1] for v in lane_data.lane_xy], '-k', lw=1)
+plt.plot([v[0] for v in lane_data.lane_el], [v[1] for v in lane_data.lane_el], '-b', lw=1)
+plt.plot([v[0] for v in lane_data.lane_er], [v[1] for v in lane_data.lane_er], '-b', lw=1)
 
 # draw sample trajectory
-for k in range(10):
-    res = compute_curve_params([20, -5, np.pi / 12, 200, 5 * (k - 5), 0])
+for k in range(9):
+    res = compute_curve_params([20, -5, np.pi / 12, 200, 5 * (k - 4), 0])
     trajectory_s = np.poly1d(res[0:6])
     trajectory_l = np.poly1d(res[6:12])
 
     points = []
-    for i in range(num):
-        xy = frenet2xy(trajectory_s(i / num), trajectory_l(i / num))
+    for i in range(lane_data.num):
+        xy = lane_data.frenet2xy(trajectory_s(i / num), trajectory_l(i / num))
         points.append(xy)
     xs = [p[0] for p in points]
     ys = [p[1] for p in points]
